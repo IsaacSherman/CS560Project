b507c6802be2761a2c329f6868595e94584b4604
= <=>
2016-02-18 17:27:43 -0500

    Sockets kind of work, but stderr needs to get redirected too.

---
+++ Filesystem/JShell/src/dupsocks.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <fcntl.h>
+
+void dupsockin(int fd)
+{
+  if (dup2(fd, 0) < 0) { perror("dupsockin:dup2"); exit(1); }
+}
+
+void dupsockout(int fd){
+if(dup2(fd, 1) <0) { perror("dupsockout:dup2"); exit(1);}
+}
