dec1e43c86ef54bd23e0d1d69d7247bf82589422
whalsey <whalsey@users.noreply.github.com>
2016-02-27 11:14:51 -0500

    Added test script

---
+++ Filesystem/Commands/exp/test.sh
@@ -0,0 +1,20 @@
+##
+# test.sh written by William Halsey
+# whalsey@vols.utk.edu
+#
+# used to run different tests for the file system
+#
+
+rm disk.disk
+
+##
+# Currently supports running only one test
+# comment the rest out
+#
+
+##
+# TESTS
+./test_mkfs
+
+#./test_mkdir
+
