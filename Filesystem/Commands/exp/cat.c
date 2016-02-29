d6cca80ba191dc52db479bca174f734bb8776439
whalsey <whalsey@users.noreply.github.com>
2016-02-28 21:26:25 -0500

    rough cat

---
+++ Filesystem/Commands/exp/cat.c
@@ -0,0 +1,52 @@
+/*
+ * cat.c written by William Halsey
+ * whalsey@vols.utk.edu
+ *
+ */
+
+#include "fs.h"
+#include "cat.h"
+
+// ========== PROTOTYPES OF "PRIVATE" FUNCTIONS ==========
+// Never call these directly
+
+
+/*
+ *
+ */
+void cat(FS_t fs, char *dir_name) {
+	int flag = 0;
+	int i;
+	
+	// Check children for matching filename
+	for (i=2; i<fs->cd->size+2; i++) {
+		if (!strncmp(dir_name, fs->cd->children[i], 16)) {
+			flag = 1;
+			break;
+		}
+	}
+	
+	if (flag && fs->cd->children[i]->itype == F) {
+		int size; 
+		FILE *f;
+		char *DATA;
+		
+		// TODO - FIX THIS!!
+		if (fs->cd->children[i]->levels = DIRECT) {
+			size = fs->cd->children[i]->size;
+			f = fopen(fs->path, "r");
+			fseek(f, (long)fs->cd->children[i]->children[1], SEEK_SET);
+			
+			DATA = (char *)malloc((size+1)*sizeof(char));
+			fread(DATA, sizeof(char), size, f);
+			DATA[size] = '\0';
+			
+			printf("%s", DATA);
+			
+			free(DATA);
+			fclose(f);
+		}
+	}
+	
+	return;
+}
---
+++ Filesystem/Commands/exp/cat.h
@@ -0,0 +1,12 @@
+/*
+ * cat.h written by William Halsey
+ * whalsey@vols.utk.edu
+ *
+ */
+
+#ifndef CAT_H
+#define CAT_H
+
+void cat(FS_t, char *);
+
+#endif
