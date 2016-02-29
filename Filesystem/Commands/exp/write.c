6e77b97f7dbb9f1cb3e08e1c2012068d97737e25
whalsey <whalsey@users.noreply.github.com>
2016-02-29 00:35:48 -0500

    write

--- Filesystem/Commands/exp/write.c
+++ Filesystem/Commands/exp/write.c
@@ -16,22 +16,17 @@
  */
 void write(FS_t fs, int index, char *string) {
 	//
-	if (index < FD_SIZE && fs->fd[index].active == 1 && fs->fd[index].mode == R) {
-		if (size + fs->fd[index].offset >= fs->fd[index].file->size)
-			size = fs->fd[index].file->size - fs->fd[index].offset;
+	if (index < FD_SIZE && fs->fd[index].active == 1 && fs->fd[index].mode == W) {
+		if (size + fs->fd[index].offset >= fs->page_size)
+			size = fs->page_size - fs->fd[index].offset;
 			
-		char *DATA = (char *)malloc((size+1)*sizeof(char));
-		FILE *f = fopen(fs->path, "r");
+		FILE *f = fopen(fs->path, "r+");
 		fseek(f, (long)fs->fd[index].file->children[1] + (long)fs->fd[index].offset, SEEK_SET);
 		
 		fs->fd[index].offset += size;
 		
-		fread(data, size, sizeof(char), f);
-		data[size] = '\0';
+		fwrite(string, size, sizeof(char), f);
 		
-		printf("%s", data);
-		
-		free(data);
 		fclose(f);
 		
 	} else
